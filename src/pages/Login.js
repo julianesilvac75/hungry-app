fe8043a087323d503d104731ef8e00572f275e0c
Millene Galetti <mikagaletti1@gmail.com>
2022-01-27 17:04:08 -0300

    Feat: Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,8 +2,35 @@ import React from 'react';
 
 function Login() {
   return (
-    <div />
+    <form>
+      <label htmlFor="email">
+        Email:
+        <input
+          type="text"
+          id="email"
+          data-testid="email-input"
+        />
+      </label>
+
+      <label htmlFor="password">
+        Senha:
+        <input
+          type="text"
+          id="password"
+          data-testid="password-input"
+        />
+      </label>
+
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Entrar
+      </button>
+    </form>
   );
 }
 
 export default Login;
+
+// const REGEX_EMAIL = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+?$/i;
