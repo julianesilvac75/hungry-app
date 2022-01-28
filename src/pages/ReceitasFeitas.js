a4084b24359f58ff1c017fd3c053d6014c704b04
Millene Galetti <mikagaletti1@gmail.com>
2022-01-28 17:27:17 -0300

    Feat: Implementa botoes de filtro

--- src/pages/ReceitasFeitas.js
+++ src/pages/ReceitasFeitas.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import FilterButtons from '../components/FilterButtons';
 
 function ReceitasFeitas() {
   return (
     <div>
       <Header titleHeader="Done Recipes" isVisible={ false } />
+      <FilterButtons />
     </div>
   );
 }
