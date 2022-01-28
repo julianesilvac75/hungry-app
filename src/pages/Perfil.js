144eaae30269d23f57305eea7bf14e0e515ed0e5
Juliane Cardoso <julianesilvac75@gmail.com>
2022-01-28 14:54:37 -0300

    Feat: Requisitos 82, 84, 85 e 86

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,6 +7,29 @@ function Perfil() {
   return (
     <div>
       <Header titleHeader="Profile" isVisible={ false } />
+      <p
+        data-testid="profile-email"
+      >
+        email
+      </p>
+      <Link
+        to="/done-recipes"
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </Link>
+      <Link
+        to="/favorite-recipes"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </Link>
+      <Link
+        to="/"
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </Link>
       <Footer />
     </div>
   );
