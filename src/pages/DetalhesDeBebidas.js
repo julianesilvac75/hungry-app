f8106011f78a5445502c59f821768fc7c2f17cc1
Juliane Cardoso <julianesilvac75@gmail.com>
2022-01-31 18:21:44 -0300

    Feat: Requisitos 33 ao 35

--- src/pages/DetalhesDeBebidas.js
+++ src/pages/DetalhesDeBebidas.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import DetailsCard from '../components/DetailsCard';
+import fetchAPI from '../services/api';
+import { URLS } from '../services/constants';
 
-function DetalhesDeBebidas() {
+function DetalhesDeBebidas({ match: { params: { id } } }) {
+  const [recipeDetails, setRecipeDetails] = useState([]);
+  console.log(recipeDetails);
+
+  function extractProperties(key) {
+    return Object.entries(recipeDetails[0])
+      .filter((entry) => entry[0].includes(key) && entry[1] !== null);
+  }
+
+  useEffect(() => {
+    const URL = URLS.drinks.detailById;
+    fetchAPI(URL(id), (data) => setRecipeDetails(data.drinks));
+  }, [id]);
+
   return (
-    <div />
+    recipeDetails.length && <DetailsCard
+      recipeDetails={ {
+        name: recipeDetails[0].strDrink,
+        image: recipeDetails[0].strDrinkThumb,
+        category: recipeDetails[0].strAlcoholic,
+        ingredients: extractProperties('Ingredient'),
+        measure: extractProperties('Measure'),
+        instructions: recipeDetails[0].strInstructions,
+      } }
+    />
   );
 }
+
+DetalhesDeBebidas.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default DetalhesDeBebidas;
