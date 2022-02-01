3f040e12fab5992f0d40725f0a9fbdf2eb8668b0
Gabriel Pinheiro <g.felippe5965@gmail.com>
2022-01-31 22:32:43 -0300

    Feat: Requisito 36

--- cypress/integration/recipe_detail_spec.js
+++ cypress/integration/recipe_detail_spec.js
@@ -141,7 +141,7 @@ describe.only('35 - Desenvolva a tela de forma que contenha uma imagem da receit
   });
 });
 
-describe('36 - Implemente as recomendações, para receitas de comida, a recomendação deverá ser bebida e vice-versa', () => {
+describe.only('36 - Implemente as recomendações, para receitas de comida, a recomendação deverá ser bebida e vice-versa', () => {
   it('Verifica se a requisição para a API de bebidas foi realizada', () => {
     cy.visit('http://localhost:3000/foods/52771', {
       onBeforeLoad(win) {
