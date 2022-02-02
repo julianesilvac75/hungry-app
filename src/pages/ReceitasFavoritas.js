e65b21cc4f0ee7590065ea485b5136c77d03cacf
André Melo <adr.smelo@gmail.com>
2022-02-02 20:54:19 -0300

    Feat: Requisitos 60 ao 66

--- src/pages/ReceitasFavoritas.js
+++ src/pages/ReceitasFavoritas.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import Header from '../components/Header';
+import FilterButtons from '../components/FilterButtons';
+import DoneFavRecipeCard from '../components/DoneFavRecipeCard';
+import AppRecipesContext from '../context/AppRecipesContext';
 
 function ReceitasFavoritas() {
+  const { favoriteRecipes } = useContext(AppRecipesContext);
+  const [filterButton, setFilterButton] = useState('All');
+
+  function filterRecipes() {
+    if (filterButton === 'All') {
+      return favoriteRecipes;
+    }
+    if (filterButton === 'Food') {
+      return favoriteRecipes.filter(({ type }) => type === 'food');
+    }
+    return favoriteRecipes.filter(({ type }) => type === 'drink');
+  }
+
   return (
     <div>
       <Header titleHeader="Favorite Recipes" isVisible={ false } />
+      <FilterButtons setFilterButton={ setFilterButton } />
+
+      {filterRecipes().map((recipe, index) => (<DoneFavRecipeCard
+        key={ recipe.id }
+        index={ index }
+        { ...recipe }
+      />)) }
     </div>
   );
 }
