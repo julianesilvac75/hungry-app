bb4342db47034ff37724bfa9deb02d2188e2d2f7
André Melo <adr.smelo@gmail.com>
2022-02-02 23:26:43 -0300

    Feat: Requisitos 75 e 76

--- src/components/IngredientsCard.jsx
+++ src/components/IngredientsCard.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const index = 2;
-
-function IngredientsCard() {
+function IngredientsCard({ name, image, index }) {
   return (
     <div>
       <button
@@ -10,14 +9,21 @@ function IngredientsCard() {
         data-testid={ `${index}-ingredient-card` }
       >
         <img
-          src=""
-          alt=""
+          style={ { width: '100px' } }
+          src={ image }
+          alt={ name }
           data-testid={ `${index}-card-img` }
         />
-        <h2 data-testid={ `${index}-card-name` }>Ingrediente</h2>
+        <h2 data-testid={ `${index}-card-name` }>{ name }</h2>
       </button>
     </div>
   );
 }
+
+IngredientsCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+};
 
 export default IngredientsCard;
