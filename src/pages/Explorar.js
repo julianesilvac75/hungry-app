975c702c743d192fb5c4c140495e3a70863e6114
Juliane Cardoso <julianesilvac75@gmail.com>
2022-02-04 18:01:43 -0300

    Styles: Explorar

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -2,23 +2,28 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import '../styles/Explorar.css';
 
 function Explorar() {
   return (
     <div>
       <Header titleHeader="Explore" isVisible={ false } />
-      <Link
-        to="/explore/foods"
-        data-testid="explore-foods"
-      >
-        Explore Foods
-      </Link>
-      <Link
-        to="/explore/drinks"
-        data-testid="explore-drinks"
-      >
-        Explore Drinks
-      </Link>
+      <div className="links-container">
+        <Link
+          to="/explore/foods"
+          className="category-button explore"
+          data-testid="explore-foods"
+        >
+          Explore Foods
+        </Link>
+        <Link
+          to="/explore/drinks"
+          className="category-button explore"
+          data-testid="explore-drinks"
+        >
+          Explore Drinks
+        </Link>
+      </div>
       <Footer />
     </div>
   );
